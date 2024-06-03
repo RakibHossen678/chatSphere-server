0f6576c6f862172e5e50cb2a7b9d8f2d53ffeaa7
RakibHossen678 <rh491464@gmail.com>
2024-06-03 20:34:12 +0600

    get all users

--- index.js
+++ index.js
@@ -233,8 +233,14 @@ async function run() {
       res.send(result);
     });
 
+    //get a user
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
-    
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
